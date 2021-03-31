2287110e8feee5693a054453258d6d453dd0465c
Barto <illya1004work@gmail.com>
2021-03-31 22:53:49 +0300

    validation user create

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const {UserInputError} = require('apollo-server');
 const {User} = require('../models')
 
 module.exports = {
@@ -10,4 +12,47 @@ module.exports = {
       }
     },
   },
+  Mutation: {
+    register: async (_, args) => {
+      let {username, email, password, confirmPassword} = args;
+      let errors = {};
+      try {
+        //TODO Validate input data
+        if (email.trim() === '') errors.email ='email must not be empty';
+        if (username.trim() === '') errors.username ='username must not be empty';
+        if (password.trim() === '') errors.password ='password must not be empty';
+        if (confirmPassword.trim() === '') errors.confirmPassword ='repeat password must not be empty';
+
+        if (password !== confirmPassword) errors.confirmPassword = 'passwords must match';
+
+        // Check if username/email exist
+        // if user not exist return null
+        const userByUsername = await User.findOne({where: {username}});
+        const userByEmail = await User.findOne({where: {email}});
+
+        if (userByUsername) errors.username = 'username is taken';
+        if (userByEmail) errors.email = 'email is taken';
+
+        if(Object.keys(errors).length > 0) {
+          throw new UserInputError('',errors);
+        }
+
+        // Hash password
+        password = await bcrypt.hash(password, 6);
+
+        // Create user
+        const user = await User.create({
+          username,
+          email,
+          password,
+        })
+
+        // Return user
+        return user;
+      } catch (err) {
+        console.error(err);
+        throw new UserInputError('Bad input',{errors: err});
+      }
+    }
+  }
 };
