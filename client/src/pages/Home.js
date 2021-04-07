0e57efe0126dec420cead663343011e3f4def1e7
Barto <illya1004work@gmail.com>
2021-04-07 19:26:07 +0300

    get users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {Row, Col, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-
+import {gql, useQuery} from '@apollo/client';
 import {useAuthDispatch} from '../context/auth';
+
+
+const GET_USERS = gql`
+query getUsers{
+    getUsers{
+        username email createdAt
+    }
+}
+`
+
+
 
 const Home = ({history}) => {
   const dispatch = useAuthDispatch();
@@ -11,6 +22,20 @@ const Home = ({history}) => {
     dispatch({type: 'LOGOUT'});
     history.push('/login')
   }
+
+  const {loading, data, error} = useQuery(GET_USERS);
+
+  if (error) {
+    console.log(error)
+  }
+  if (data) {
+    console.log(data)
+  }
+  if (loading) {
+    console.log(loading)
+  }
+
+
 
   return (
     <Row className="bg-white justify-content-around">
